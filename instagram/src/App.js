8415400efd4997277c29481365ea65bd11ee2237
novinary <novina.dev@gmail.com>
2019-07-26 23:59:57 +0100

    Implemented PostsContainer, PostHeader & PostImage

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import dummyData from './dummy-data';
 import SearchBar from './components/SearchBar/SearchBarContainer';
+import PostsContainer from './components/PostsContainer/PostsContainer';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: dummyData
+    };
+  }
+
   render() {
     return (
       <div className="App">
       <SearchBar />
+      <PostsContainer posts={this.state.posts} />
       </div>
     );
   }
