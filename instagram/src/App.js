c8f3e938e5cc455620a9b77b22009514c98bb104
novinary <novina.dev@gmail.com>
2019-07-26 23:44:47 +0100

    Implemented SearchBarContainer component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBarContainer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <SearchBar />
       </div>
     );
   }
