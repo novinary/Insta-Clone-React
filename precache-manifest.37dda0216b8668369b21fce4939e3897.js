e458dbcd50c1b03d6ed5812582a6dda9ea8c837d
novinary <novina.dev@gmail.com>
2019-07-28 23:33:36 +0100

    Updates

---
+++ precache-manifest.37dda0216b8668369b21fce4939e3897.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5b04d8ab846ed1b2e28c18455f6152",
+    "url": "/Insta-Clone-React/index.html"
+  },
+  {
+    "revision": "ec8abfe629a5ba073dda",
+    "url": "/Insta-Clone-React/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e4ec2e3a33034d5d4d57",
+    "url": "/Insta-Clone-React/static/css/main.2f6ed517.chunk.css"
+  },
+  {
+    "revision": "ec8abfe629a5ba073dda",
+    "url": "/Insta-Clone-React/static/js/2.25ef7976.chunk.js"
+  },
+  {
+    "revision": "e4ec2e3a33034d5d4d57",
+    "url": "/Insta-Clone-React/static/js/main.99be7799.chunk.js"
+  },
+  {
+    "revision": "12a680a1b26db3e7ac34",
+    "url": "/Insta-Clone-React/static/js/runtime~main.d6275e71.js"
+  },
+  {
+    "revision": "dab84e5ae5a049d075fcd49ee11bc39a",
+    "url": "/Insta-Clone-React/static/media/igcameralogo.dab84e5a.png"
+  },
+  {
+    "revision": "ff854302b1aeaf008c572d7d015a0317",
+    "url": "/Insta-Clone-React/static/media/iglogo.ff854302.png"
+  }
+]);
